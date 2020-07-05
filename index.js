cf9662ea76f53f8e32bb989770b354ab6e210f97
Sunny <30768018+sunny52525@users.noreply.github.com>
2020-07-06 03:59:02 +0530

    fixed algorithm

--- index.js
+++ index.js
@@ -46,10 +46,13 @@ var matrix = [
 ]
 
 // MINIMAX Algorithm Credit : Geeks For Geeks
-function isMovesLeft(board) {
-    for (var i = 0; i < 3; ++i) {
-        for (var j = 0; j < 3; ++j) {
-            if (board[i][j] == -1)
+
+var player =1,opponent=0;
+// MINIMAX Algorithm Credit : Geeks For Geeks
+function isMovesLeft(board){
+    for(var i =0 ; i<3 ;i++){
+        for(var j =0 ; j<3 ;j++){
+            if(board[i][j]==-1)
                 return true;
         }
     }
@@ -57,39 +60,39 @@ function isMovesLeft(board) {
 }
 
 
-function evaluate(b) {
+function evaluate(b){
     //Checking for victory (row)
-    for (var row = 0; row < 3; ++row) {
-        if (b[row][0] == b[row][1] && b[row][1] == b[row][2]) {
+    for(var row =0 ; row<3;row++){
+        if(b[row][0]==b[row][1] && b[row][1]==b[row][2]){
 
-            if (b[row][0] == 0)
-                return +10;
-            else if (b[row][0] == 1)
+            if (b[row][0] == player) 
+                return 10;
+            else if (b[row][0] == opponent) 
                 return -10;
         }
     }
     //checking for victory (column)
-    for (var col = 0; col < 3; ++col) {
+    for(var col =0 ; col< 3 ;col++){
 
-        if (b[0][col] == b[1][col] && b[1][col] == b[2][col]) {
-            if (b[0][col] == 0)
-                return +10;
-            else if (b[0][col] == 1)
+        if (b[0][col] == b[1][col] &&  b[1][col] == b[2][col]) {
+            if (b[0][col] == player) 
+                return 10;
+            else if (b[0][col] == opponent) 
                 return -10;
         }
     }
 
     //Diagonals
     if (b[0][0] == b[1][1] && b[1][1] == b[2][2]) {
-        if (b[0][0] == 0)
-            return +10;
-        else if (b[0][0] == 1)
+        if (b[0][0] == player)
+             return 10;
+         else if (b[0][0] == opponent) 
             return -10;
     }
     if (b[0][2] == b[1][1] && b[1][1] == b[2][0]) {
-        if (b[0][2] == 0)
-            return +10;
-        else if (b[0][2] == 1)
+        if (b[0][2] == player) 
+            return 10; 
+        else if (b[0][2] == opponent) 
             return -10;
     }
 
@@ -98,76 +101,77 @@ function evaluate(b) {
 
 }
 
-function minimax(board, depth, isMax) {
+function minimax(board,depth,isMax){
 
-    var score = evaluate(board);
+    var score=evaluate(board);
+    // console.log(score);
     // Maximizer Won
-    if (score == 10)
+    if(score==10) 
         return score;
     //Minimizer Won
-    if (score == -10)
+    if(score==-10)
         return score;
 
     // No One Won Yet
-    if (isMovesLeft(board) == false)
+    if(isMovesLeft(board)==false)
         return 0;
-
-
-    if (isMax) {
-        var best = -1000
-        for (var i = 0; i < 3; ++i) {
-            for (var j = 0; j < 3; ++j) {
-                if (board[i][j] == -1) {
-
-                    board[i][j] = 0;
+    
+    
+    if(isMax){
+        var best=-1000
+        for(var i = 0 ; i<3 ;i++){
+            for(var j  = 0 ; j<3 ;j++){
+                if(board[i][j]==-1){
+                    
+                    board[i][j]=player;
 
                     //Call MiniMax recursively to chose max Value
-                    best = Math.max(best, minimax(board, depth + 1), !isMax)
-
-                    board[i][j] = -1;
+                    best=Math.max(best,minimax(board,depth+1,!isMax));
+                    
+                    board[i][j]=-1;
                 }
             }
         }
         return best;
-    } else {
-        var best = 1000
-        for (var i = 0; i < 3; ++i) {
-            for (var j = 0; j < 3; ++j) {
-                if (board[i][j] == -1) {
-
-                    board[i][j] = 1;
+    } else{
+        var best=1000
+        for(var i = 0 ; i<3 ;i++){
+            for(var j  = 0 ; j<3 ;j++){
+                if(board[i][j]==-1){
+                    
+                    board[i][j]=opponent;
 
                     //Call MiniMax recursively to chose max Value
-                    best = Math.min(best, minimax(board, depth + 1), !isMax)
-
-                    board[i][j] = -1;
+                    best=Math.min(best,minimax(board,depth+1,!isMax));
+                    
+                    board[i][j]=-1;
                 }
             }
         }
         return best;
     }
-
+  
 }
 
 
 function bestMove(board) {
-    var bestVal = -1000;
-    var moves = [-1, -1];
-    for (var i = 0; i < 3; ++i) {
-        for (var j = 0; j < 3; ++j) {
+    var bestVal=-1000;
+    var moves=[-1,-1];
+    for(var i =0 ; i < 3 ; i++){
+        for(var j =0 ; j < 3 ; j++){
             //if cell is empty
-            if (board[i][j] == -1) {
+            if(board[i][j] == -1){
                 //make this move
-                board[i][j] = 0;
+                board[i][j]=player;
+                
+                var moveVal= minimax(board,0,false);
 
-                var moveVal = minimax(board, 0, false);
+                board[i][j]=-1;
 
-                board[i][j] = -1;
-
-                if (moveVal > bestVal) {
-                    moves[0] = i;
-                    moves[1] = j;
-                    bestVal = moveVal
+                if(moveVal>bestVal){
+                    moves[0]=i;
+                    moves[1]=j;
+                    bestVal=moveVal
                 }
 
             }
@@ -175,7 +179,6 @@ function bestMove(board) {
     }
     return moves
 }
-
 /**
  * MINIMAX ALGORITHM ENDS HERE
  */
@@ -293,17 +296,11 @@ function playGame(clickedCell) {
                 gameOver = true;
                 $(".verdict").text("You broke the game(haha,somethings wrong,If you reached here then pls report the moves)");  return;
             }
-            if(numberOfMoves==0){
-                var best =[0,2];
-                matrix[0][2]=1;
-                botmove(best);
-
-            }else
-            {var best = bestMove(matrix);
+            var best = bestMove(matrix);
             
             console.log("Best move was"+best);
             matrix[best[0]][best[1]] = 1;
-            botmove(best);}
+            botmove(best);
         }
     } else if (clickedCell == '7') {
         if (matrix[2][0] != -1) {
